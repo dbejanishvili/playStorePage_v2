771589e0962feea5dc3581c2ef4b6fd7ef18840c
dbejanishvili <datobejanishvili@gmail.com>
2019-03-28 23:32:08 +0400

    init

--- app/src/main/java/com/example/playstorepage_v2/MainActivity.java
+++ app/src/main/java/com/example/playstorepage_v2/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.playstorepage_v2;
 
+import android.content.res.TypedArray;
+import android.media.Image;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int filledStarId = R.drawable.icons8_star_filled_500;
+    private int emptyStarId = R.drawable.icons8_star_500;
+    private TypedArray ratingStars = getResources().obtainTypedArray(R.array.ratingStarImages);
+
     private View.OnClickListener defaultButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -17,19 +25,63 @@ public class MainActivity extends AppCompatActivity {
             snackbar.show();
         }
     };
+
+    private void updateRatingBar(int rating){
+        for(int i=0;i<=rating;i++){
+            ImageView star = findViewById(ratingStars.getResourceId(i,-1));
+            star.setImageResource(filledStarId);
+        }
+        for(int i=rating+1;i<=4;i++){
+            ImageView star = findViewById(ratingStars.getResourceId(i,-1));
+            star.setImageResource(emptyStarId);
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Button uninstallButton = (Button) findViewById(R.id.uninstallButton);
-        uninstallButton.setOnClickListener(defaultButtonListener);
-        final Button openButton = (Button) findViewById(R.id.openButton);
-        openButton.setOnClickListener(defaultButtonListener);
-        final Button travelAndLocalButton = (Button) findViewById(R.id.travelAndLocalButton);
-        travelAndLocalButton.setOnClickListener(defaultButtonListener);
-        final Button similarButton = (Button) findViewById(R.id.similarButton);
-        similarButton.setOnClickListener(defaultButtonListener);
-        final Button readMoreButton = (Button) findViewById(R.id.readMoreButton);
-        readMoreButton.setOnClickListener(defaultButtonListener);
+        addButtonListeners();
+        addRatingBarListeners();
+    }
+
+    private void addButtonListeners() {
+        findViewById(R.id.uninstallButton).setOnClickListener(defaultButtonListener);
+        findViewById(R.id.openButton).setOnClickListener(defaultButtonListener);
+        findViewById(R.id.travelAndLocalButton).setOnClickListener(defaultButtonListener);
+        findViewById(R.id.similarButton).setOnClickListener(defaultButtonListener);
+        findViewById(R.id.readMoreButton).setOnClickListener(defaultButtonListener);
+    }
+
+    private void addRatingBarListeners() {
+        findViewById(R.id.ratingStar1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateRatingBar(0);
+            }
+        });
+        findViewById(R.id.ratingStar2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateRatingBar(1);
+            }
+        });
+        findViewById(R.id.ratingStar3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateRatingBar(2);
+            }
+        });
+        findViewById(R.id.ratingStar4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateRatingBar(3);
+            }
+        });
+        findViewById(R.id.ratingStar5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateRatingBar(4);
+            }
+        });
     }
 }
